1792d36050134240608510de9bcad2963611678e
auxa <contact@auxa.me>
2023-08-17 21:01:48 +0900

    進捗

--- src/components/PrefecturesList.tsx
+++ src/components/PrefecturesList.tsx
@@ -26,12 +26,19 @@ function CheckboxItem (props: CheckboxItemProps) {
 export default function PrefecturesList(){
 
     //GET https://opendata.resas-portal.go.jp/api/v1/prefecturesする
-    // useEffect(() => {
-    //     axios.get('https://opendata.resas-portal.go.jp/api/v1/prefectures')
-    // },[])
-    const [prefectures, setPrefectures] = useState([
-        {prefName: "北海道", prefCode: 1, checked: false},
-    ]);
+    // const defaultPref = {prefName: "北海道", prefCode: 1, checked: false};
+    const [prefectures, setPrefectures] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://opendata.resas-portal.go.jp/api/v1/prefectures', { headers: { 'X-API-KEY': process.env.REACT_APP_RESAS_API_KEY } })
+        .then(response => {
+            console.log(response);
+        })
+        .catch((error) => {
+            console.log('失敗');
+            console.log(error.status);
+          });
+    },[])
 
     const onChange = (checked: boolean, value: number) => {
         const newPrefectures = prefectures.map((prefecture) => {
@@ -48,6 +55,7 @@ export default function PrefecturesList(){
 
     return (
         <div className="checkbox-list">
+            <h3>都道府県</h3>
             {prefectures.map((prefecture) => (
                 <CheckboxItem key={prefecture.prefCode}
                     checked={prefecture.checked}
